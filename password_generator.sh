4061946306983c4cd9e95ef6dd6208bc962fefbd
Vishnu Rathore <vr8043836@gmail.com>
2023-03-24 20:18:41 +0530

    2nd project

--- password_generator.sh
+++ password_generator.sh
@@ -5,13 +5,14 @@ echo "welcome to password genrator"
 sleep 3
 
 echo "Enter the length of the password"
-#user inputs
 
+#user inputs
 read PASS_LENGTH
 
 for p in $( seq 1 ):
 
 do
+#bae64 is used for encoding from number to characters
 	openssl rand -base64 48 | cut  -c1-$PASS_LENGTH
 
 done
---
+++ system_info_scripts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "enter your name"
+
+read name
+echo "hi $name please find your system informatio"
+
+sleep 3
+echo " * CURRENT DATE AND TIME ***************** "
+date | awk '{print " Today is: " $3 " "$2"; Day = " $1 " ; Time: " $4 }'
+ 
+sleep 3
+
+echo " *****available disk space****"
+df -H | xargs | awk '{ print " Disk Space Available: " "Free/Used: " $10 "/" $9 " :GB" }'
+
+
+sleep 3
+echo " user uptime"
+uptime
+
+
+echo  "last 3 login details"
+last | head -3
+
+sleep 3
+
+echo " currently connected "
+w
